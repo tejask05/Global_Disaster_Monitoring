034dea0c1a8c4bca4ae8f53e76dc01010de318ea
ARYANRVIMPADAPU <106469555+ARYANRVIMPADAPU@users.noreply.github.com>
2024-03-13 14:29:11 +0530

    Update newsapi.py

--- newsapi.py
+++ newsapi.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import folium
 from streamlit_folium import st_folium
 from datetime import datetime, timedelta, timezone
 from pymongo import MongoClient
