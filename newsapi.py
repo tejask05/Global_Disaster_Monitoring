f7be9926237869a0947f22fb53dff381f6767c74
ARYANRVIMPADAPU <106469555+ARYANRVIMPADAPU@users.noreply.github.com>
2024-03-19 14:05:05 +0530

    Update newsapi.py

--- newsapi.py
+++ newsapi.py
@@ -24,7 +24,6 @@ collection = db["testing"]
 df = pd.DataFrame(list(collection.find()))
 
 # Convert the 'timestamp' column to datetime
-df['timestamp'] = pd.to_datetime(df['timestamp'],errors='coerce')
 
 # Setting page configuration to occupy the entire width
 st.set_page_config(layout="wide")
